4b1970f771a91dd1715fab56a47065fbe57bf137
Tarek Oukacha <tarekoukasha2009@gmail.com>
2025-03-23 13:27:23 +0400

    2D shapes using python

---
+++ main.py
@@ -0,0 +1,10 @@
+import math
+
+#calculate the ceiling value of the given number
+
+num1=math.ceil(556.3322)
+print(num1)
+#calculate the flloor value of the given number
+
+num2=math.floor(4.22)
+print(num2)
