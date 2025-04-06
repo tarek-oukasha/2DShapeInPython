88c40079765479676c28c86c776b3038de87b754
Tarek Oukacha <tarekoukasha2009@gmail.com>
2025-04-06 13:21:03 +0400

    2d shapes importing math functions like square rooting and max and min

--- main.py
+++ main.py
@@ -8,3 +8,18 @@ print(num1)
 
 num2=math.floor(4.22)
 print(num2)
+
+#write the code to calculate the square root of a number
+num3=144
+for i in range (num3):
+    if i*i == num3:
+        print(f"the square root of {num3} is {i}")
+
+num4=math.sqrt(9)
+print(num4)
+
+num5=math.pow(3,4)
+print(num5)
+
+num6=math.pi
+print(num6)
---
+++ main1.py
@@ -0,0 +1,12 @@
+import math
+"""write a program to take two numbers as an input from the user and calculate the 
+max and min of two numbers"""
+
+num1=int(input("what is the first number you would like to write"))
+num2=int(input("what is the second number you would like to write"))
+
+maximum=max(num1,num2)
+minimum=min(num1,num2)
+
+print(maximum)
+print(minimum)
